74ced4b1eadc63c25ac018bc11a2d1389d0251a4
Piotr <piotr.w.dabrowski2@gmail.com>
2021-11-30 22:58:49 +0100

    Pole Kwadratu

--- NowyPlik.py
+++ NowyPlik.py
@@ -2,4 +2,10 @@ def pole_prostokata(a, b):
     pole = a * b
     return pole
 
+def pole_kwadratu(a):
+    pk = pole_prostokata(a, a)
+    return pk
+
 print ("Pole prostokąta " + str(pole_prostokata(3, 4)))
+
+print ("pole kwadratu " + str(pole_kwadratu(3)))
